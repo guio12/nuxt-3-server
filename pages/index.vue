8475b8111d5d9b10433ad5dcbf8c4990d3ff4791
guio12 <guillaume.audouze@hakisa.com>
2024-02-21 15:02:38 +0100

    lessons 3-4-5-6 - API, errors, server routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-
+const { data: pokemon, error } = await useFetch('/api/popk')
 </script>
 
 <template>
   <div>
     <h1>Home page for Nuxt 3 server course</h1>
+
+    <ul v-if="pokemon">
+      <li>
+        <img :src="pokemon?.sprite" alt="pokemon image" />
+      </li>
+      <li>{{ pokemon?.id }} {{ pokemon?.name }}</li>
+    </ul>
+
+    <div v-else>
+      <p>{{ error }}</p>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
